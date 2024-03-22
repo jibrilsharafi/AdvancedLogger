c321a69dd01f0a16c1691f9d560d88e247402250
Jibril Sharafi <jibril.sharafi@gmail.com>
2024-03-22 06:57:36 +0100

    Improved example with more functions and comments

--- examples/basicUsage/basicUsage.cpp
+++ examples/basicUsage/basicUsage.cpp
@@ -1,18 +1,35 @@
 /*
-  * This is a simple example to show how to use the AdvancedLogger library.
-  * 
-  * The library is designed to be easy to use and to be able to log messages
-  * to the console and to a file on the SPIFFS.
-  * 
-  * You can set the print level and save level, and the library will only log
-  * messages accordingly.
-*/
+ * File: basicUsage.cpp
+ * --------------------
+ * This file provides a simple example to show how to use the AdvancedLogger library.
+ * 
+ * Author: Jibril Sharafi, @jibrilsharafi
+ * Date: 21/03/2024
+ * GitHub repository: https://github.com/jibrilsharafi/AdvancedLogger
+ *
+ * This library is licensed under the MIT License. See the LICENSE file for more information.
+ *
+ * The AdvancedLogger library provides advanced logging for the ESP32.
+ * It allows you to log messages to the console and to a file on the SPIFFS.
+ * You can set the print level and save level, and the library will only log
+ * messages accordingly.
+ *
+ * Possible logging levels:
+ * - ADVANCEDLOGGER_VERBOSE
+ * - ADVANCEDLOGGER_DEBUG
+ * - ADVANCEDLOGGER_INFO
+ * - ADVANCEDLOGGER_WARNING
+ * - ADVANCEDLOGGER_ERROR
+ */
 #include <Arduino.h>
 #include <SPIFFS.h>
 
 #include "advancedLogger.h"
 
 AdvancedLogger logger;
+
+String printLevel;
+String saveLevel;
 
 void setup() {
     Serial.begin(9600);
@@ -36,6 +53,10 @@ void loop() {
     logger.log("This is an error message!!!!", "simpleExample::loop", ADVANCEDLOGGER_ERROR);
     logger.log("This is an fatal message!!!!!", "simpleExample::loop", ADVANCEDLOGGER_FATAL);
     delay(1000);
+    logger.logOnly("This is an info message (logOnly)!!", "simpleExample::loop", ADVANCEDLOGGER_INFO);
+
+    printLevel = logger.getPrintLevel();
+    saveLevel = logger.getSaveLevel();
 
     if (millis() > 60000) {
       logger.clearLog();
