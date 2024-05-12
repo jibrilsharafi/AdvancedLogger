ea7fbaa07724f481c61bfbd8dbbbab44f63cda02
Jibril Sharafi <83494924+jibrilsharafi@users.noreply.github.com>
2024-05-12 12:54:08 +0200

    Arduino compliancy (#9)
    * Cleanup merge mess, renamed files
    
    * Renamed files
    
    * Update keywords
    
    * Update arduino-lint.yml to work only on PRs

--- examples/basicUsage/basicUsage.ino
+++ examples/basicUsage/basicUsage.ino
@@ -1,5 +1,5 @@
 /*
- * File: basicServer.cpp
+ * File: basicUsage.cpp
  * --------------------
  * This file provides a simple example to show how to use the AdvancedLogger library.
  *
@@ -9,15 +9,10 @@
  *
  * This library is licensed under the MIT License. See the LICENSE file for more information.
  *
- * This example is a simple web server that serves a webpage with a button
- * that sends the user to the /log page, where the logs are displayed, and
- * another button that sends the user to the /config page, where the configuration
- * is displayed.
- *
- * Remember to change the ssid and password to your own.
- *
- * Tested only on the ESP32. For other boards, you may need to change the
- * WebServer library.
+ * The AdvancedLogger library provides advanced logging for the ESP32.
+ * It allows you to log messages to the console and to a file on the SPIFFS.
+ * You can set the print level and save level, and the library will only log
+ * messages accordingly.
  *
  * Possible logging levels:
  * - ADVANCEDLOGGER_VERBOSE
@@ -29,10 +24,8 @@
  */
 #include <Arduino.h>
 #include <SPIFFS.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
 
-#include "advancedLogger.h"
+#include "AdvancedLogger.h"
 
 String customLogPath = "/customPath/log.txt";
 String customConfigPath = "/customPath/config.txt";
@@ -42,8 +35,6 @@ String customLogPath = "/customPath/log.txt";
 String customConfigPath = "/customPath/config.txt";
 
 AdvancedLogger logger(customLogPath.c_str(), customConfigPath.c_str()); // Leave empty for default paths
-
-AsyncWebServer server(80);
 
 String printLevel;
 String saveLevel;
@@ -64,10 +55,6 @@ const long intervalLogClear = 30000;
 
 int maxLogLines = 10; // Low value for testing purposes
 
-// **** CHANGE THESE TO YOUR SSID AND PASSWORD ****
-const char *ssid = "YOUR_SSID";
-const char *password = "YOUR_PASSWORD";
-
 void setup()
 {
     // Initialize Serial and SPIFFS (mandatory for the AdvancedLogger library)
@@ -80,7 +67,7 @@ void setup()
     }
 
     logger.begin();
-
+    
     // Setting the print and save levels is not mandatory.
     // If you don't set them, the default levels are first taken
     // from the SPIFFS file, and if it doesn't exist, the default
@@ -90,39 +77,17 @@ void setup()
     // Set the maximum number of log lines before the log is cleared
     // If you don't set this, the default is used
     logger.setMaxLogLines(maxLogLines);
+    logger.log("AdvancedLogger setup done!", "basicUsage::setup", ADVANCEDLOGGER_INFO);
     // Set the maximum number of log lines before the log is cleared
     // If you don't set this, the default is used
     logger.setMaxLogLines(maxLogLines);
-    logger.log("AdvancedLogger setup done!", "basicServer::setup", ADVANCEDLOGGER_INFO);
-
-    // Connect to WiFi
-    // --------------------
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(1000);
-        logger.log("Connecting to WiFi..", "basicServer::setup", ADVANCEDLOGGER_INFO);
-    }
-    logger.log(("IP address: " + WiFi.localIP().toString()).c_str(), "basicServer::setup", ADVANCEDLOGGER_INFO);
-
-    // Serve a simple webpage with a button that sends the user to the page /log and /config
-    // --------------------
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-              { request->send(200, "text/html", "<button onclick=\"window.location.href='/log'\">Explore the logs</button><br><br><button onclick=\"window.location.href='/config'\">Explore the configuration</button>"); });
-    server.serveStatic("/log", SPIFFS, customLogPath.c_str());
-    server.serveStatic("/config", SPIFFS, customConfigPath.c_str());
-    server.serveStatic("/log", SPIFFS, customLogPath.c_str());
-    server.serveStatic("/config", SPIFFS, customConfigPath.c_str());
-    server.onNotFound([](AsyncWebServerRequest *request)
-                      { request->send(404, "text/plain", "Not found"); });
-    server.begin();
-    logger.log("Server started!", "basicServer::setup", ADVANCEDLOGGER_INFO);
+    logger.log("AdvancedLogger setup done!", "basicUsage::setup", ADVANCEDLOGGER_INFO);
 
     lastMillisLogDump = millis();
     lastMillisLogClear = millis();
     lastMillisLogDump = millis();
     lastMillisLogClear = millis();
-    logger.log("Setup done!", "basicServer::setup", ADVANCEDLOGGER_INFO);
+    logger.log("Setup done!", "basicUsage::setup", ADVANCEDLOGGER_INFO);
 }
 
 void loop()
@@ -138,7 +103,6 @@ void loop()
     logger.log("This is a fatal message!!!!!", "basicServer::loop", ADVANCEDLOGGER_FATAL);
     delay(500);
     logger.logOnly("This is an info message (logOnly)!!", "basicServer::loop", ADVANCEDLOGGER_INFO);
-    delay(1000);
     delay(1000);
 
     printLevel = logger.getPrintLevel();
@@ -176,6 +140,7 @@ void loop()
         logger.log("Log cleared!", "basicServer::loop", ADVANCEDLOGGER_WARNING);
 
         lastMillisLogClear = millis();
+        logger.log("Log cleared!", "basicServer::loop", ADVANCEDLOGGER_WARNING);
 
         lastMillisLogClear = millis();
     }
